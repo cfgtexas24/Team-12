e55b49e5c5cb1336a384d8077219137d97da08af
ZoraLah <arozlala@gmail.com>
2024-10-18 23:08:30 -0500

    added financial-literacy.js

---
+++ mobile-web-app/src/data/financial-literacy.js
@@ -0,0 +1,72 @@
+export const financialList = [{
+    find: 'lesson 1',
+    title: 'A Beginners Guide to Financial Education',
+    author: "asdfasdf",
+    description: "This video is a beginners guide to financial literacy. Financial literacy is the knowledge and skills to make smart money decisions in your personal finances. Financial education is the same as financial literacy.",
+    type: "YouTube",
+    url: 'https://www.youtube.com/embed/4XZIv4__sQA?si=2bMFgDD-O-2isTee'
+  }, {
+    find: 'lesson 2',
+    title: 'You and Your Money',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67802',
+    text: 'Let’s get started. Bobby Seagull introduces the course in the first video and outlines some of the ways your money becomes more important as you come to the end of school or college. In this session you will explore banking and saving, and why you need skills in managing your money. It’s all about choosing to keep safe and take control.'  
+  }, {
+    find: 'lesson 3',
+    title: 'Earning Money, Understanding Tax',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67800',
+    text: 'As you start to earn an income from part-time or holiday work you’ll want to understand why some of what you earn can be deducted in the form of Income Tax and National Insurance Contributions (NICs). What are these, when do you have to pay them and how do you claim back tax if you’ve paid more than you should? What does the government do with the Income Tax and National Insurance that it raises? As you’ll learn here, a substantial proportion of government spending is on benefits for various groups within the population. Might you be eligible for any benefits?'  
+  }, {
+    find: 'lesson 4',
+    title: 'Being a Savvy Shopper and Building a Budget',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67803',
+    text: 'In this session the focus turns first to spending and the factors that determine our spending habits. You then start to build a simple budget. This is going to skill you up to manage your personal finances effectively in the period before you leave home.'  
+  }, {
+    find: 'lesson 5',
+    title: 'Preparing for Life After School',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67821',
+    text: 'Your focus in this session is reaching that key point in your life where you decide what you want to do when your schooldays are over. Whatever choice you make, it will have financial consequences for you – especially as this is normally when wider responsibilities for financial management are passed from your parents to you.'  
+  }, {
+    find: 'lesson 6',
+    title: 'Living in Shared Households',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67859',
+    text: 'From the age of 18 it’s likely that you’ll start living away from home for at least part of the year, either to study or to start a job in another part of the country. As a student, even if you spend your first year in student accommodation, you’ll probably move into rented accommodation with other students at some point. For your first job, even if it’s close enough for you to stay in the family home, as time moves on there’s a high chance you’ll want to move out, particularly if you’re in a long-term relationship. Although the timeline varies, and whatever the reason for your move away, there’s a high chance you’ll find yourself living in a shared rental with people of your own age. This means changes to your budget, and it’s these changes that you focus on here.'  
+  }, {
+    find: 'lesson 7',
+    title: 'Understanding Debt and How to Borrow Wisely',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67860',
+    text: 'In this session you look at what’s involved in borrowing (and repaying) money, at how interest rates work, at the financial institutions that are involved in setting rates, and at how debt can be used sensibly to support your goals and aspirations.'  
+  }, {
+    find: 'lesson 8',
+    title: 'Good Credit and How to Keep it',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67862',
+    text: 'You’ve looked at the institutions that lend money, the various ways money can be borrowed and at what determines the cost of borrowing. You’ve also looked at when borrowing is rational and when it is foolish. In this session you extend your analysis of debt and explore how to integrate the cost of managing your debts within your budget. You take a look at the activities of the credit reference agencies and at how you can manage your financial profile to maximize the credit score assigned to you.'  
+  }, {
+    find: 'lesson 9',
+    title: 'Planning Now for Later in Your Life',
+    author: "asdfasdf",
+    type: "website",
+    url: 'https://www.open.edu/openlearn/mod/oucontent/view.php?id=67875',
+    description: 'The earlier you start to plan and save for your pension the better the outcome will be for you in later life when you retire from work. Starting a pension plan in your early 20s make sense. If you leave it until you’re in your 40s and 50s you risk being in income poverty when you retire. Here you’ll learn more about the risks and returns from different types of investment.'  
+  }, {
+    find: 'lesson 10',
+    title: 'Wrapping Up - A Comprehensive Video about Personal Finance',
+    author: "asdfasdf",
+    description: "Is it possible to maintain a comfortable standard of living despite having a little income? Are the risks associated with investing in the stock market for regular people manageable? The answer of all these questions lies in one thing. That is financial literacy. With Financial literacy all that is possible. The thing is you don’t need to inherit a lot of money or having high salary to live a financially free life, rather the answer lies in managing your finances better and efficiently.And how one can do that.",
+    type: "YouTube",
+    url: 'https://www.youtube.com/embed/0kwl10ftLm0?si=F3wI1ZZN4yXMR9P3'
+  }];
+  
