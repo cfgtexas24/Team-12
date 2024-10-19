2a55aa419ba0488c0734650a39f2f6d66ce88764
Cathy Hou <cathyhou@college.harvard.edu>
2024-10-18 20:07:00 -0400

    checked emergency route works

---
+++ mobile-web-app/src/pages/EmergencyContact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmergencyContact = () => {
+  return (
+    <div>
+      <h1>Emergency Contact Information</h1>
+      <p>This is where you provide emergency contact details.</p>
+    </div>
+  );
+};
+
+export default EmergencyContact;
